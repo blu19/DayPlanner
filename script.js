8c71b721692c1fc6f6f4daac213dc8e0949c4d84
BrianU <brian.ulatowski@gmail.com>
2020-02-01 22:17:40 -0800

    html has inputs and save buttons and added for loop for hours

---
+++ script.js
@@ -0,0 +1,11 @@
+var rightNow = moment().hour(23);
+
+console.log(rightNow);
+
+//for loop for calender rows
+for (var time = 9; time < 18; time++) {
+    //console.log(time)
+    var i = time - 9;
+    console.log(i)
+    //attribute color change if less than
+}
