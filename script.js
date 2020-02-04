39b00aad9725b7cae216917c051632ed4e840bd4
BrianU <brian.ulatowski@gmail.com>
2020-02-03 22:48:49 -0800

    change inputs to textarea

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
+//display current date and time
 var moment = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $('#showTime').text(moment);
 //document.getElementById("#headerText").innerHTML = moment;
 //$('.city').html(`<h1>${response.name} Weather Details</h1>`);
 console.log(moment);
 
-//for loop for calender rows
-for (var time = 9; time < 18; time++) {
-    //console.log(time)
-    var i = time - 9;
-    console.log(i)
-    //attribute color change if less than
-}
-//calender rows
-$('#myContainerPlanner').append('<div id="nine" class="input-group mb-3 mx-auto row plannerRow hour-index" style="width: 700px;"></div>');
-$('#nine').append("<div class='col-md-2'>");
-$('#nine').append('<span class="timeContainer">');
+//$('.btn').on('click', savePlan);
 
-//Showing hour and am or pm for each row
-var displayHour = 0;
-var ampm = "";
+$('#saves9').click(function() {
+    var savePlan = $('#inputNine').val();
+    console.log(savePlan);
+    localStorage.setItem('9am, savePlan');
+});
 
-if (hour > 12) {
-    displayHour = hour - 12;
-    ampm = "pm";
-} else {
-    displayHour = hour;
-    ampm = "am";
-}
+// function savePlan() {
+//     var plans = $('#inputNine').val();
+//     localStorage.setItem('nine', JSON.stringify(plans));
+// };
+
+// function storedPlans() {
+//     $('#inputNine').val(localStorage.getItem('nine'));
+//     console.log(localStorage.getItem('nine');
+// }
+
+
+// storedPlans();
